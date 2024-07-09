b5b8964320bf60067287ff46f44a0c82bee37a79
blue <67697646+m256i@users.noreply.github.com>
2024-07-10 00:14:02 +0200

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <cstdio> /*  puts() */
+
+/* the main function of our WSL rewrite. Written with best performance and security in mind whilst keeping perfect backwards compatibility in classic Microsoft fashion! */
+int main()
+{
+    puts("[warn] start_kernel: Function not implemented");
+    return -1;
+}
