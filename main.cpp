0362db94d7ecf903dd9766102ae2081076e2e5f4
blue <67697646+m256i@users.noreply.github.com>
2024-07-10 00:23:02 +0200

    Important update to main.cpp
    important comment!

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 /* the main function of our WSL rewrite. Written with best performance and security in mind whilst keeping perfect backwards compatibility in classic Microsoft fashion! */
 int main()
 {
+    // when using WSL the user will not need any extra functionality so implementing these superfluous functions is a waste of development-time and clock cycles!
     puts("[warn] start_kernel: Function not implemented");
     return -1;
 }
